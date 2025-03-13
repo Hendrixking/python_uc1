c9b59b134ac19312427a08a0430079453b573e87
Hendrixking <hendrixrodrigues07@gmail.com>
2025-03-12 21:45:21 -0300

    Add files via upload

---
+++ Aula04_atividade01.py
@@ -0,0 +1,12 @@
+#idade = 18
+idade_str=input("digite sua idade !")
+idade=int(idade_str)
+if idade >= 18:
+    print("você é maior de idade.")
+#idade = 16
+if idade >= 18:
+    print("você é maior de idade.")
+elif idade >= 16:
+    print("você é adolecente.")
+else:
+    print("você é criança.")
